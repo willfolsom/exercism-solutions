f8a712ca5b878c16d1fde2d1e4c191af849c2346
Will Folsom <folsomwg@gmail.com>
2021-04-30 18:55:06 -0400

    leap

---
+++ go/leap/leap.go
@@ -0,0 +1,7 @@
+// Package leap contains a function for calculating Leap Year
+package leap
+
+// IsLeapYear takes an input year and returns true or false, based on if its a leap year.
+func IsLeapYear(year int) bool {
+	return (year % 4 == 0 && year % 100 != 0) || year % 400 == 0
+}
