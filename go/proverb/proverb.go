9c95c5fbcca511cdb4b84beea7ca23c422bde7a7
Will Folsom <folsomwg@gmail.com>
2021-04-30 14:16:10 -0400

    cleanup proverb

--- go/proverb/proverb.go
+++ go/proverb/proverb.go
@@ -4,15 +4,14 @@ package proverb
 // Proverb returns a structured proverb based on an input list of strings.
 func Proverb(rhyme []string) []string {
 	proverb := []string{}
-	if len(rhyme) == 0 {
-		return proverb
-	}
 
-	for i, item := range rhyme {
-		if i < len(rhyme) - 1 {
-			proverb = append(proverb, getStandardLine(item, rhyme[i+1]))
-		} else {
-			proverb = append(proverb, getLastLine(rhyme[0]))
+	if len(rhyme) != 0 {
+		for i, item := range rhyme {
+			if i < len(rhyme) - 1 {
+				proverb = append(proverb, getStandardLine(item, rhyme[i+1]))
+			} else {
+				proverb = append(proverb, getLastLine(rhyme[0]))
+			}
 		}
 	}
 
