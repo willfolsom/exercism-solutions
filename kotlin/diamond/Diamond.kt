f705300999cac638e9ffe43ecb13cb1e671d6317
Will Folsom <folsomwg@gmail.com>
2021-06-06 23:11:55 -0400

    diamond

---
+++ kotlin/diamond/Diamond.kt
@@ -0,0 +1,28 @@
+class DiamondPrinter {
+    fun printToList(input: Char): List<String> {
+        var list = arrayListOf<String>();
+        val distance = input - 'A';
+        var d = distance;
+        var x = 1;
+        val space = " ";
+
+        var spaceString = "";
+        for (i in 'A'..input) {
+            var its = i.toString();
+            if (i == 'A') {
+                if (distance > 0) {
+                    spaceString = space.repeat(distance);
+                }
+                list.add(spaceString + its + spaceString);
+            } else {
+                d-=1;
+                spaceString = space.repeat(x);
+                var outerSpaceString = space.repeat(d);
+                list.add(outerSpaceString + its + spaceString + its + outerSpaceString);
+                x+=2;
+            }
+        }
+
+        return list.slice(0..distance) + list.slice(0..distance-1).reversed();
+    }
+}
